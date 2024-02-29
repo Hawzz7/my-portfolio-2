1c90339448278f6290d0de446e5610dbf585f9b9
Hawzz7 <vicky.007.cr@gmail.com>
2024-03-01 01:13:51 +0530

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ const App = () => {
   //put the style tag in the div tag to change the background image to custom one
   // style={{backgroundImage: "url('src/assets/galaxy.png')"}}
   return (
-    <div className="flex flex-col w-full h-full bg-star-image">
+    <div 
+    style={{backgroundImage: "url('src/assets/galaxy.png')"}}
+    className="flex flex-col w-full h-full bg-">
       <Navbar />
       <Home />
       <About />
